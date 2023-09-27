88e694ffc40e42b45fe2e326d9218f0e81900a56
ASxCRO <antonio.suups@gmail.com>
2023-09-27 21:19:07 +0200

    end of section 4

--- client/src/app/layout/App.tsx
+++ client/src/app/layout/App.tsx
@@ -1,8 +1,8 @@
-import Catalog from "../../features/catalog/Catalog";
 import { Container, CssBaseline, createTheme } from "@mui/material";
 import Header from "./Header";
 import { ThemeProvider } from "@emotion/react";
 import { useState } from "react";
+import { Outlet } from "react-router-dom";
 
 function App() {
     const [darkMode, setDarkMode] = useState(false);
@@ -23,7 +23,7 @@ function App() {
                 <CssBaseline />
                 <Header checked={darkMode} setDarkMode={setDarkMode} />
                 <Container>
-                    <Catalog />
+                    <Outlet />
                 </Container>
             </ThemeProvider>
         </>
--- client/src/app/layout/Header.tsx
+++ client/src/app/layout/Header.tsx
@@ -1,18 +1,88 @@
-import { AppBar, Switch, Toolbar, Typography } from "@mui/material";
+import { ShoppingCart } from "@mui/icons-material";
+import { AppBar, Badge, Box, IconButton, List, ListItem, Switch, Toolbar, Typography } from "@mui/material";
+import { NavLink } from "react-router-dom";
+
+const midLinks = [
+    {
+        title: 'catalog', path: '/catalog',
+    },
+    {
+        title: 'about', path: '/about',
+    },
+    {
+        title: 'contact', path: '/contact',
+    }
+]
+
+const rightLinks = [
+    {
+        title: 'login', path: '/login',
+    },
+    {
+        title: 'register', path: '/register',
+    },
+]
+
+const navStyle = {
+    color: 'inherit',
+    typography: 'h6',
+    '&:hover': {
+        color: 'grey.500'
+    },
+    '&.active': {
+        color: 'text.secondary'
+    }
+}
 
 interface Props {
     checked: boolean;
-    setDarkMode: () => void;
+    setDarkMode: (checked:boolean) => void;
 }
 
 export default function Header({ checked, setDarkMode} : Props) {
     return (
         <AppBar position='static' sx={{mb:4} }>
-            <Toolbar>
-                <Typography variant='h6'>
-                    RE-STORE
-                </Typography>
-                <Switch checked={checked} onChange={() => setDarkMode(!checked) } /> 
+            <Toolbar sx={{display:'flex', justifyContent:'space-between' , alignItems: 'center'} }>
+                <Box sx={{ display: 'flex',  alignItems: 'center' } }>
+                    <Typography variant='h6' component={NavLink} to='/'
+                        sx={navStyle}
+                    >
+                        RE-STORE
+                    </Typography>
+                    <Switch checked={checked} onChange={() => setDarkMode(!checked)} /> 
+                </Box>
+                <List sx={{display:'flex'}}>
+                    {midLinks.map(({ title, path }) => (
+                        <ListItem
+                            component={NavLink}
+                            to={path}
+                            key={path}
+                            sx={navStyle}
+                        >
+                            {title.toUpperCase() }
+                        </ListItem>
+                    ))}
+                </List>
+                <Box sx={{ display: 'flex', alignItems: 'center' }}>
+                    <IconButton size='large' edge='start' color='inherit' sx={{ mr: 2 }}>
+                        <Badge badgeContent='4' color='secondary'>
+                            <ShoppingCart />
+                        </Badge>
+                    </IconButton>
+                    <List sx={{ display: 'flex' }}>
+                        {rightLinks.map(({ title, path }) => (
+                            <ListItem
+                                component={NavLink}
+                                to={path}
+                                key={path}
+                                sx={navStyle}
+                            >
+                                {title.toUpperCase()}
+                            </ListItem>
+                        ))}
+                    </List>
+                </Box>
+                
             </Toolbar>
         </AppBar>
     )
